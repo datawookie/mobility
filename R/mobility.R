67d43e164c409a67bf9872e06563033779b0e6c2
Andrew Collier <andrew@fathomdata.dev>
2021-07-19 11:42:29 +0200

    feat: Build data from regional files

--- R/mobility.R
+++ R/mobility.R
@@ -1,11 +1,12 @@
-MOBILITY_CSV <- here::here("data-raw/mobility-global.csv")
 MOBILITY_RDA <- here::here("data/mobility.rda")
 
 if (!file.exists(MOBILITY_RDA)) {
   message("Updating ", MOBILITY_RDA, ".")
-  mobility <- read.csv(MOBILITY_CSV)
+  mobility <- purrr::map_dfr(
+    list.files(here::here("data-raw"), pattern = "*.csv", full.names = TRUE),
+    function(path) {
+      read.csv(path)
+    }
+  )
   save(mobility, file = MOBILITY_RDA)
 }
-
-print(file.info(MOBILITY_CSV))
-print(file.info(MOBILITY_RDA))
--- scripts/build-data.R
+++
@@ -1,3 +0,0 @@
-mobility <- read.csv(here::here("data-raw/mobility-global.csv"))
-
-usethis::use_data(mobility, overwrite = TRUE)
