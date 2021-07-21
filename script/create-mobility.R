e7d29696c7a07f722a62b842c8621963a9645446
Andrew Collier <andrew@fathomdata.dev>
2021-07-21 09:31:09 +0300

    feat: Use {readr}-2.0.0 to read multiple CSV in parallel

--- script/create-mobility.R
+++ script/create-mobility.R
@@ -1,13 +1,16 @@
 MOBILITY_RDA <- here::here("data/mobility.rda")
-dir.create(dirname(MOBILITY_RDA))
+DATA_DIR <- dirname(MOBILITY_RDA)
+
+if (!dir.exists(DATA_DIR)) {
+  message("Creating data directory.")
+  dir.create(DATA_DIR)
+}
+
+MOBILITY_CSV <- list.files(here::here("data-raw"), pattern = "*.csv", full.names = TRUE)
 
 if (!file.exists(MOBILITY_RDA)) {
+  message("Reading CSV files.")
+  mobility <- readr::read_csv(MOBILITY_CSV)
   message("Updating ", MOBILITY_RDA, ".")
-  mobility <- purrr::map_dfr(
-    list.files(here::here("data-raw"), pattern = "*.csv", full.names = TRUE),
-    function(path) {
-      read.csv(path)
-    }
-  )
   save(mobility, file = MOBILITY_RDA)
 }
