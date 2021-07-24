5d1ff07fb6760cda73150e2db64b56435fe12d61
Andrew Collier <andrew@fathomdata.dev>
2021-07-24 08:28:19 +0300

    feat: Rename columns

--- script/create-mobility.R
+++ script/create-mobility.R
@@ -10,7 +10,24 @@ MOBILITY_CSV <- list.files(here::here("data-raw"), pattern = "*.csv", full.names
 
 if (!file.exists(MOBILITY_RDA)) {
   message("Reading CSV files.")
-  mobility <- readr::read_csv(MOBILITY_CSV)
+  mobility <- readr::read_csv(MOBILITY_CSV, show_col_types = FALSE)
+  message("Renaming columns.")
+  mobility <- mobility %>%
+    select(
+      iso_3166_1 = country_region_code,
+      iso_3166_2 = iso_3166_2_code,
+      country = country_region,
+      region = sub_region_1,
+      sub_region = sub_region_2,
+      metro = metro_area,
+      census_fips = census_fips_code,
+      everything(),
+      -place_id
+    ) %>%
+    rename_at(
+      vars(ends_with("from_baseline")),
+      function(n) sub("_percent_change_from_baseline", "", n)
+    )
   message("Updating ", MOBILITY_RDA, ".")
   save(mobility, file = MOBILITY_RDA)
 }
